c70c00fde674c53be18a752fccde28e02eba4516
nfitzgerald <nicholas.fitzgerald@gmail.com>
2016-05-08 12:41:26 -0700

    converted to exact calc

--- bb_skill_sampler.py
+++ bb_skill_sampler.py
@@ -20,9 +20,11 @@ class Action:
 # Do one sample of a sequence of actions, given a player with a certain set of skills. Returns true if we
 # successfully reach the end of the sequence, return false if we fail somewhere along the way.
 def do_sequence(sequence, skills, has_trr, can_use_trr_next = True):
+    sequence = list(sequence)
+    skills = set(skills)
     if sequence == []:
         # We have successfully reached the end of the sequence
-        return True
+        return 1.0
 
     curr_action = sequence.pop(0)
 
@@ -30,22 +32,21 @@ def do_sequence(sequence, skills, has_trr, can_use_trr_next = True):
         raise Exception("Next action is not of type Action: " + curr_action)
 
     #Do a die roll for hte currect action. If sucessful, we can continue with the sequence.
-    die_roll = random.randint(1, 6)
+    chance_of_success = float(6 - curr_action.roll_required + 1) / 6
 
-    if (die_roll >= curr_action.roll_required):
-        return do_sequence(sequence, skills, has_trr)
+    chance_after_success = do_sequence(sequence, skills, has_trr)
 
     # Use skills if we have them. Remove the skill from the set of skills (since it can no longer be used)
     # and repeat the sequence.
     if curr_action.action_type == ActionType.dodge and Skill.dodge in skills:
-        resolve_skill(Skill.dodge, skills, sequence, curr_action, has_trr)
-    if curr_action.action_type == ActionType.gfi and Skill.sure_feet in skills:
-        resolve_skill(Skill.sure_feet, skills, sequence, curr_action, has_trr)
-    if curr_action.action_type == ActionType.pickup and Skill.sure_hands in skills:
-        resolve_skill(Skill.sure_hands, skills, sequence, curr_action, has_trr)
+        chance_after_failure = resolve_skill(Skill.dodge, skills, sequence, curr_action, has_trr)
+    elif curr_action.action_type == ActionType.gfi and Skill.sure_feet in skills:
+        chance_after_failure = resolve_skill(Skill.sure_feet, skills, sequence, curr_action, has_trr)
+    elif curr_action.action_type == ActionType.pickup and Skill.sure_hands in skills:
+        chance_after_failure = resolve_skill(Skill.sure_hands, skills, sequence, curr_action, has_trr)
 
     # If we reach here we have failed the roll
-    if has_trr and can_use_trr_next and (curr_action.trr_strat == TeamRerollStrategy.always or (curr_action.trr_strat == TeamRerollStrategy.after_pro and not Skill.pro in skills)):
+    elif has_trr and can_use_trr_next and (curr_action.trr_strat == TeamRerollStrategy.always or (curr_action.trr_strat == TeamRerollStrategy.after_pro and not Skill.pro in skills)):
         # Attempting to use team reroll.
         sequence.insert(0, curr_action)
 
@@ -54,15 +55,18 @@ def do_sequence(sequence, skills, has_trr, can_use_trr_next = True):
             sequence.insert(0, Action(ActionType.loner, 4, TeamRerollStrategy.never))
 
         # Redo the sequence, minus the used team reroll.
-        return do_sequence(sequence, skills, False)
-    elif Skill.pro in skills:
+        chance_after_failure = do_sequence(sequence, skills, False, can_use_trr_next = False)
+    elif Skill.pro in skills and can_use_trr_next:
         # Add a pro-check before redoing the action check.
         sequence.insert(0, curr_action)
         sequence.insert(0, Action(ActionType.pro, 4, TeamRerollStrategy.always))
-        return do_sequence(sequence, skills, has_trr, True)
+        skills.remove(Skill.pro)
+        chance_after_failure = do_sequence(sequence, skills, has_trr, True)
+    else:
+        chance_after_failure = 0.0
 
     # If we reach here, we have failed the action with no additional recourse.
-    return False
+    return (chance_of_success * chance_after_success) + ((1 - chance_of_success) * chance_after_failure)
 
 # Apply the free skill reroll by removing the skill (so it can't be used again),
 # and adding the current action back to the start of the sequence. If there is a
@@ -74,6 +78,8 @@ def resolve_skill(skill, skills, sequence, curr_action, has_trr):
 
 # Do N samples of a sequence, and return the percentage of success.
 def sample_sequence(sequence, skills = set(), has_trr = False, N = 100000):
+
+
     successes = 0.0
     for i in xrange(N):
         curr_seq = list(sequence)
--- main.py
+++ main.py
@@ -2,16 +2,17 @@ from bb_skill_sampler import *
 
 def main():
     sequence = []
-    sequence.append(Action(ActionType.dodge, 2))
+    sequence.append(Action(ActionType.dodge, 3))
     sequence.append(Action(ActionType.pickup, 4))
     sequence.append(Action(ActionType.dodge, 2))
-    sequence.append(Action(ActionType.gfi, 2))
 
     skills = set()
+    skills.add(Skill.dodge)
+    skills.add(Skill.pro)
 
     has_trr = True
 
-    print sample_sequence(sequence, skills = skills, has_trr = has_trr)
+    print do_sequence(sequence, skills = skills, has_trr = has_trr)
 
 if __name__ == '__main__':
     main()
