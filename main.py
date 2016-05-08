2fdb56d2ff980da0b18b10fbdfa2a0b01b01ed12
nfitzgerald <nicholas.fitzgerald@gmail.com>
2016-05-08 14:22:18 -0700

    added rain

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import string
 
 def main():
 
-    print "Team Reroll?\tDodge\tSure Hands\tSure Feet\tLoner\tPro\tTwo Heads\tExtra Arms\tPrehensize Tail (defender)\tBlizzard\tAgi\tBreak Tackle Strength"
+    print "Team Reroll?\tDodge\tSure Hands\tSure Feet\tLoner\tPro\tTwo Heads\tExtra Arms\tPrehensize Tail (defender)\tBlizzard\tRain\tAgi\tBreak Tackle Strength"
 
     for trr in [True, False]:
         for dodge in [True, False]:
@@ -16,54 +16,58 @@ def main():
                                     for extra_arms in [True, False]:
                                         for prehensile_tail in [True, False]:
                                             for blizzard in [True, False]:
-                                                for break_tackle in xrange(0, 7):
-                                                    if break_tackle > 0 and break_tackle <= agi:
-                                                        continue
+                                                for rain in [True, False]:
+                                                    for break_tackle in xrange(0, 7):
+                                                        if break_tackle > 0 and break_tackle <= agi:
+                                                            continue
+                                                        if rain and blizzard:
+                                                            #impossible
+                                                            continue
 
-                                                    base_dodge = 7 - agi - 1
-                                                    if two_heads:
-                                                        base_dodge -= 1
-                                                    if prehensile_tail:
-                                                        base_dodge += 1
+                                                        base_dodge = 7 - agi - 1
+                                                        if two_heads:
+                                                            base_dodge -= 1
+                                                        if prehensile_tail:
+                                                            base_dodge += 1
 
-                                                    first_dodge = base_dodge - min(0, break_tackle - agi)
+                                                        first_dodge = base_dodge - min(0, break_tackle - agi)
 
-                                                    base_pickup = 7 - agi
-                                                    if extra_arms:
-                                                        base_pickup -= 1
+                                                        base_pickup = 7 - agi
+                                                        if extra_arms:
+                                                            base_pickup -= 1
 
-                                                    if blizzard:
-                                                        gfi = 3
-                                                    else:
-                                                        gfi = 2
+                                                        if blizzard:
+                                                            gfi = 3
+                                                        else:
+                                                            gfi = 2
 
-                                                    skills = set()
-                                                    if dodge:
-                                                        skills.add(Skill.dodge)
-                                                    if sure_hands:
-                                                        skills.add(Skill.sure_hands)
-                                                    if sure_feet:
-                                                        skills.add(Skill.sure_feet)
-                                                    if loner:
-                                                        skills.add(Skill.loner)
-                                                    if pro:
-                                                        skills.add(Skill.pro)
+                                                        skills = set()
+                                                        if dodge:
+                                                            skills.add(Skill.dodge)
+                                                        if sure_hands:
+                                                            skills.add(Skill.sure_hands)
+                                                        if sure_feet:
+                                                            skills.add(Skill.sure_feet)
+                                                        if loner:
+                                                            skills.add(Skill.loner)
+                                                        if pro:
+                                                            skills.add(Skill.pro)
 
-                                                    sequenceA = []
-                                                    sequenceA.append(Action(ActionType.dodge, first_dodge + 1))
-                                                    sequenceA.append(Action(ActionType.pickup, base_pickup))
-                                                    sequenceA.append(Action(ActionType.dodge, base_dodge))
-                                                    seqA_chance = do_sequence(sequenceA, skills, trr)
+                                                        sequenceA = []
+                                                        sequenceA.append(Action(ActionType.dodge, first_dodge + 1))
+                                                        sequenceA.append(Action(ActionType.pickup, base_pickup))
+                                                        sequenceA.append(Action(ActionType.dodge, base_dodge))
+                                                        seqA_chance = do_sequence(sequenceA, skills, trr)
 
-                                                    sequenceB = []
-                                                    sequenceB.append(Action(ActionType.dodge, first_dodge))
-                                                    sequenceB.append(Action(ActionType.pickup, base_pickup))
-                                                    sequenceB.append(Action(ActionType.dodge, base_dodge))
-                                                    sequenceB.append(Action(ActionType.gfi, gfi))
-                                                    seqB_chance = do_sequence(sequenceB, skills, trr)
+                                                        sequenceB = []
+                                                        sequenceB.append(Action(ActionType.dodge, first_dodge))
+                                                        sequenceB.append(Action(ActionType.pickup, base_pickup))
+                                                        sequenceB.append(Action(ActionType.dodge, base_dodge))
+                                                        sequenceB.append(Action(ActionType.gfi, gfi))
+                                                        seqB_chance = do_sequence(sequenceB, skills, trr)
 
-                                                    ystrings = map(lambda x: "Y" if x else "N", [trr, dodge, sure_hands, sure_feet, loner, pro, two_heads, extra_arms, prehensile_tail, blizzard])
-                                                    print string.join(ystrings, '\t') + "\t%d\t%d\t%f\t%f"%(agi, break_tackle, seqA_chance, seqB_chance)
+                                                        ystrings = map(lambda x: "Y" if x else "N", [trr, dodge, sure_hands, sure_feet, loner, pro, two_heads, extra_arms, prehensile_tail, blizzard, rain])
+                                                        print string.join(ystrings, '\t') + "\t%d\t%d\t%f\t%f"%(agi, break_tackle, seqA_chance, seqB_chance)
 
 
 
